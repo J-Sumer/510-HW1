af64af6f837515ae106dd958c86c825991164dc0
Jyothi Sumer <59790162+J-Sumer@users.noreply.github.com>
2022-08-30 15:56:28 -0400

    Updating test cases so that they will pass

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ function add(a, b) {
 
 describe('Testing add function', function () {
     it('Sum of 2 and 3 should be 5', function (done) {
-        expect(add(2, 3)).to.equal(1);
+        expect(add(2, 3)).to.equal(5);
         done();
     })
 })
